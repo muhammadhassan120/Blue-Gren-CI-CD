6bb74c6c252a8d5bb495429ecd3bff2e5ee8ba2f
HammadMuqaddam <shaikhnomi580@gmail.com>
2026-04-05 12:46:05 +0500

    dummy commit

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! HELLO ';
+    return 'Hello World! HELLO from blue-green-elb';
   }
 }
